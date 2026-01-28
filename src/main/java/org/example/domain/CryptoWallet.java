ea8335fb630447a1caea1014b2e915022dd86560
dltmdrbs321@kw.ac.kr <dltmdrbs321@naver.com>
2026-01-28 12:04:20 +0900

    initial16

--- src/main/java/org/example/domain/CryptoWallet.java
+++ src/main/java/org/example/domain/CryptoWallet.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "wallets", catalog = "mock_crypto")
+@Table(name = "wallets")
 public class CryptoWallet {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
