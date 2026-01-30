c5421be10728d59ab46b65459c7b551f1029f010
dltmdrbs321@kw.ac.kr <dltmdrbs321@naver.com>
2026-01-30 19:21:10 +0900

    initial29

--- src/main/java/org/example/config/RestTemplateConfig.java
+++ src/main/java/org/example/config/RestTemplateConfig.java
@@ -9,7 +9,7 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        // 타임아웃 설정 (Java 8 스타일)
+        // 타임아웃 설정
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000); // 연결 5초 대기
         factory.setReadTimeout(5000);    // 데이터 읽기 5초 대기
